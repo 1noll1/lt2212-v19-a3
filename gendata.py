2247defacd3a97e0cb4cf81c4ac345d117f6c054
Linnea Strand <gusstrlip@GU.GU.SE@mltgpu.flov.gu.se>
2019-03-23 17:29:56 +0100

    Add -POS option

--- gendata.py
+++ gendata.py
@@ -13,6 +13,8 @@ parser.add_argument("-N", "--ngram", metavar="N", dest="ngram", type=int, defaul
 parser.add_argument("-S", "--start", metavar="S", dest="startline", type=int,
                     default=1,
                     help="What line of the input data file to start from. Default is 0, the first line.")
+parser.add_argument("-POS", "--parts-of-speech", dest="pos", action='store_true',
+                    help="If you want the data represented as part-of-speech tags rathers than word tokens.")
 parser.add_argument("-E", "--end", metavar="E", dest="endline",
                     type=int, default=None,
                     help="What line of the input data file to end on. Default is None, whatever the last line is.")
@@ -25,8 +27,6 @@ parser.add_argument("testlines", type=int, default=10,
 
 args = parser.parse_args()
 
-print("Loading data from file {}.".format(args.inputfile))
-print("Starting from line {}.".format(args.startline))
 if args.endline:
     print("Ending at line {}.".format(args.endline))
 else:
@@ -45,13 +45,16 @@ num_range = random.sample(num_range, args.testlines)
 print("Test sentences on lines: {}".format(num_range))
 
 def collect_data(): 
-    '''Starting symbol: <s>
+    '''Start symbol: <s>
     If line is among the T random lines in num_list, add it to the test data.
     Otherwise add it to training.
     '''
     vocab = ['<s>']
     testlines = []
     trainlines = []
+
+    print("Loading data from file {}.".format(args.inputfile))
+    print("Starting from line {}.".format(args.startline))
 
     with open(args.inputfile) as f:
         position = 0 #keep track of line index
@@ -61,8 +64,12 @@ def collect_data():
         while position < args.endline:
             line = f.readline()
             position += 1
-            line = [word.split("/")[0] for word in line.split()] #all cred to user Kevin@StackOverflow: https://stackoverflow.com/questions/15365046/python-removing-pos-tags-from-a-txt-file
-            vocab.extend(line)
+            if args.pos:
+                line = [word.split("/")[1] for word in line.split()]
+                vocab.extend(line)
+            else:
+                line = [word.split("/")[0] for word in line.split()] #all cred to user Kevin@StackOverflow: https://stackoverflow.com/questions/15365046/python-removing-pos-tags-from-a-txt-file
+                vocab.extend(line)
             if position in num_range: #select lines to use as test data
                 testlines.extend(line)
             else:
@@ -81,10 +88,7 @@ def encode_onehot(vocab):
     for word in list(one_hot): #iterate over vocabulary keys while it's changing
         i = word_index[word]
         #print(one_hot[word])
-        one_hot[word][i] = 1
-
-    for array_ in one_hot:
-        array_ = list(array_) 
+        one_hot[word][i] = 1 
 
     return one_hot
 
@@ -92,7 +96,6 @@ def construct_vectors(text, one_hot, N=None):
     '''Create one hot encoded ngrams:
     [hot+hot, class]
     '''
-    #print(text)
     print("Constructing {}-gram model.".format(args.ngram))
     N = args.ngram
     grams = ngrams(text, N, pad_left=True, pad_right=False, left_pad_symbol='<s>')
@@ -100,7 +103,6 @@ def construct_vectors(text, one_hot, N=None):
     onehot_vectors = []
 
     for gram in list(grams):
-        #print(gram)
         wordy = gram[-1]
         vector = []
         for w in gram[:-1]:
@@ -111,14 +113,13 @@ def construct_vectors(text, one_hot, N=None):
     return onehot_vectors
 
 def gen_output(vector, j):
-    filename = args.outputfile + '_{}'.format(j)
+    filename = args.outputfile + '_{}.csv'.format(j)
     print('Generating data frame...')
     data = pd.DataFrame(vector)
  
     print("Writing table to {}.".format(filename))
     data.to_csv(filename, index=False)
     
-
 vocab, trainlines, testlines = collect_data()
 one_hot = encode_onehot(vocab)
 
